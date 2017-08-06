9462168ae2882d1d62aef936bbafd67648c960aa
jisu An <ajs7270@naver.com>
2017-08-07 00:26:58 +0900

    add registrants to index page

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 $conn = mysqli_connect("localhost","root","mango");
 mysqli_select_db($conn,"mango");
 $result = mysqli_query($conn, "SELECT * FROM device");
-
- ?>
+?>
 
 <!DOCTYPE html>
 <html>
@@ -20,7 +19,7 @@ $result = mysqli_query($conn, "SELECT * FROM device");
     <nav>
       <ul>
         <?php
-          echo '<li><a href="http://mango.vos.io:81/registration.php">등록</a></li>';
+          echo '<li><a href="http://mango.vos.io:81/index.php?setting=1">등록</a></li>';
           echo '<li>내 장치</li>';
           while($row=mysqli_fetch_assoc($result))
           {
@@ -37,13 +36,22 @@ $result = mysqli_query($conn, "SELECT * FROM device");
       <?php
       if(empty($_GET['num'])===false)
       {
-        $sql='SELECT * FROM device WHERE id='.$_GET['id'];
+        $sql='SELECT * FROM device WHERE num='.$_GET['num'];
 	      $result=mysqli_query($conn,$sql);
 	      $row=mysqli_fetch_assoc($result);
         echo '<h2>'.$row['name'].'</h2>';
         echo '<a href="http://mango.vos.io:81/process.php?state=on"><input type="button" value="on"></a>';
         echo '<a href="http://mango.vos.io:81/process.php?state=off"><input type="button" value="off"></a>';
       }
+
+      if(empty($_GET['setting'])===false){	
+       	echo '<form class="" action="reg_process.php" method="post">';
+        echo  '<p>기기이름 : <input type="text" name="name" value=""></p>';
+        echo  '<p>기기정보 : <input type="text" name="signal" value=""></p>';
+        echo  '<input type="submit" name="submit">';
+        echo '</form>';
+      }
+
        ?>
      </nav>
     </article>
