1d57af9cb9fdaaca67014f64ef9aed7a26d446d6
owlsogul <owlsogul@naver.com>
2017-08-08 15:52:23 +0900

    들여쓰기 전반적인 수정 @index.php

--- index.php
+++ index.php
@@ -34,44 +34,44 @@ $result = mysqli_query($conn, "SELECT * FROM device");
 
     <article>
       <?php
-      if(empty($_GET['id'])===false)
-      {
-        $sql='select * from device where id='.$_GET['id'];
-        $result=mysqli_query($conn,$sql);
-        $row=mysqli_fetch_assoc($result);
+        if(empty($_GET['id'])===false)
+        {
+          $sql='select * from device where id='.$_GET['id'];
+          $result=mysqli_query($conn,$sql);
+          $row=mysqli_fetch_assoc($result);
 
-        echo '<h2>'.$row['name'].'</h2>';
-        echo '<a href="process.php?state=on"><input type="button" value="on"></a>';
-        echo '<a href="process.php?state=off"><input type="button" value="off"></a>';
-      }
+          echo '<h2>'.$row['name'].'</h2>';
+          echo '<a href="process.php?state=on"><input type="button" value="on"></a>';
+          echo '<a href="process.php?state=off"><input type="button" value="off"></a>';
+        }
 
-      if(empty($_GET['setting'])===false){
-		$sql='SELECT * FROM device';
-		$result=mysqli_query($conn,$sql);
-        // echo '<input type="submit" name ="bringDeviceSignal" value="신호가져오기"/>';
-		// 현재 기기출력 및 수정
-    while($row=mysqli_fetch_assoc($result)){
-      echo '<form id="device_manage_info" class="" action="update_process.php" method="post">';
-      echo  '기기id   : '.$row['id'].'<br>';
-      echo  '기기이름 : <input type="text" name="name" value="'.$row['name'].'"><br>';
-      echo  '기기정보 : <input type="text" name="description" value="'.$row['description'].'"><br>';
-      echo  '기기상태 : ';
-      echo   $row['status']==1 ? 'On' : 'Off';
-      echo  '<br>추가시간 : '.$row['created'];
-      echo  '<br><input type="submit" name="submit" value="변경">';
+      if(empty($_GET['setting'])===false){ // 기기 관리 페이지를 눌렀을 때
+    		$sql='SELECT * FROM device';
+    		$result=mysqli_query($conn,$sql);
+        //echo '<input type="submit" action="bring_device_signal.php" name ="bringDeviceSignal" value="신호가져오기"/>';
+    		// 현재 기기출력 및 수정
+        while($row=mysqli_fetch_assoc($result)){
+          echo '<form id="device_manage_info" class="" action="update_process.php" method="post">';
+          echo  '기기id   : '.$row['id'].'<br>';
+          echo  '기기이름 : <input type="text" name="name" value="'.$row['name'].'"><br>';
+          echo  '기기정보 : <input type="text" name="description" value="'.$row['description'].'"><br>';
+          echo  '기기상태 : ';
+          echo   $row['status']==1 ? 'On' : 'Off';
+          echo  '<br>추가시간 : '.$row['created'];
+          echo  '<br><input type="submit" name="submit" value="변경">';
+          echo '</form>';
+        }
+
+  		//기기 추가
+      echo '<form class="" action="reg_process.php" method="post">';
+      echo  '<h2>기기추가</h2>';
+      echo  '기기이름 : <input type="text" name="name" value="'.$row['name'].'">';
+      echo  '정보 : <input type="text" name="description" value="'.$row['description'].'">';
+      echo  '<input type="submit" name="submit" value="추가">';
       echo '</form>';
-    }
+  	}
 
-		//기기 추가
-    echo '<form class="" action="reg_process.php" method="post">';
-    echo  '<h2>기기추가</h2>';
-    echo  '기기이름 : <input type="text" name="name" value="'.$row['name'].'">';
-    echo  '정보 : <input type="text" name="description" value="'.$row['description'].'">';
-    echo  '<input type="submit" name="submit" value="추가">';
-    echo '</form>';
-		}
-
-       ?>
+    ?>
     </article>
   </body>
 </html>
