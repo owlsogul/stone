2eecbbc34218c4a170cfff04405518864304e25d
owlsogul <owlsogul@naver.com>
2017-08-08 16:55:14 +0900

    minggu (#2)
    * add basic css
    
    * 상단 메뉴 가로로 정렬
    
    * 들여쓰기 수정
    
    * 기기 정보 폼 수정 @기기관리 @index.php
    
    * 들여쓰기 전반적인 수정 @index.php
    
    * 신호가져오기 버튼 추가 @index.php
    
    * 신호가져오기 버튼 @index.php
    
    * 신호 보내기 버튼 여백 정상화 @index.php
    
    * 상단 메뉴 디자인 수정 @index.php
    
    상단메뉴
    블랙. 사이즈 축소
    
    * 텍스트 조금 수정 @index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $result = mysqli_query($conn, "SELECT * FROM device");
     </header>
 
     <nav>
-      <ul>
+      <ul id="menubar">
         <?php
           echo '<li><a href="index.php?setting=true">기기관리</a></li>';
           echo '<li>내 장치</li>';
@@ -34,45 +34,56 @@ $result = mysqli_query($conn, "SELECT * FROM device");
 
     <article>
       <?php
-      if(empty($_GET['id'])===false)
-      {
-        $sql='select * from device where id='.$_GET['id'];
-	    $result=mysqli_query($conn,$sql);
-	    $row=mysqli_fetch_assoc($result);
-        
-		echo '<h2>'.$row['name'].'</h2>';
-        echo '<a href="process.php?state=on"><input type="button" value="on"></a>';
-        echo '<a href="process.php?state=off"><input type="button" value="off"></a>';
-      }
+        if(empty($_GET['id'])===false)
+        {
+          $sql='select * from device where id='.$_GET['id'];
+          $result=mysqli_query($conn,$sql);
+          $row=mysqli_fetch_assoc($result);
 
-      if(empty($_GET['setting'])===false){	
-		$sql='SELECT * FROM device';
-		$result=mysqli_query($conn,$sql);
-        // echo '<input type="submit" name ="bringDeviceSignal" value="신호가져오기"/>';
-		// 현재 기기출력 및 수정
-		while($row=mysqli_fetch_assoc($result))
-          {
-        	echo '<form class="" action="update_process.php" method="post">';
-        	echo  '기기id : '.$row['id'].', ';
-			echo  '기기이름 : <input type="text" name="name" value="'.$row['name'].'">';
-       		echo  '정보 : <input type="text" name="description" value="'.$row['description'].'">';
-        	echo  '상태 : ';
-			echo   $row['status']==1 ? 'On' : 'Off';
-			echo  '추가시간 : '.$row['created'];
-			echo  '<input type="submit" name="submit" value="변경">';
-        	echo '</form>';
-          }
+          echo '<h2>'.$row['name'].'</h2>';
+          echo '<a href="process.php?state=on"><input type="button" value="on"></a>';
+          echo '<a href="process.php?state=off"><input type="button" value="off"></a>';
+        }
 
-		//기기 추가
-		echo '<form class="" action="reg_process.php" method="post">';
-        echo  '<h2>기기추가</h2>';
-		echo  '기기이름 : <input type="text" name="name" value="'.$row['name'].'">';
-       	echo  '정보 : <input type="text" name="description" value="'.$row['description'].'">';
-		echo  '<input type="submit" name="submit" value="추가">';
+      if(empty($_GET['setting'])===false){ // 기기 관리 페이지를 눌렀을 때
+    		$sql='SELECT * FROM device';
+    		$result=mysqli_query($conn,$sql);
+        echo '<form id="device_manage_signal_form" action="bring_device_signal.php" method="post">';
+        echo '<input type="submit" name ="bringDeviceSignal" value="신호가져오기"/>';
         echo '</form>';
-		}
 
-       ?>
+
+        echo '<h2>연결된 기기 목록</h2>';
+
+        echo '<h4>새로운 기기 목록</h4>';
+        echo '로딩중...';
+
+    		// 현재 기기출력 및 수정
+        // 이미 저장 되어 있는 기기일 경우
+        echo '<h4>저장된 기기 목록</h4>';
+        while($row=mysqli_fetch_assoc($result)){
+          echo '<form id="device_manage_info" class="" action="update_process.php" method="post">';
+          echo  '기기id   : '.$row['id'].'<br>';
+          echo  '기기이름 : <input type="text" name="name" value="'.$row['name'].'"><br>';
+          echo  '기기정보 : <input type="text" name="description" value="'.$row['description'].'"><br>';
+          echo  '기기상태 : ';
+          echo   $row['status']==1 ? 'On' : 'Off';
+          echo  '<br>추가시간 : '.$row['created'];
+          echo  '<br><input type="submit" name="submit" value="변경">';
+          echo '</form>';
+        }
+
+
+  		//기기 추가
+      echo '<form class="" action="reg_process.php" method="post">';
+      echo  '<h2>기기추가</h2>';
+      echo  '기기이름 : <input type="text" name="name" value="'.$row['name'].'">';
+      echo  '정보 : <input type="text" name="description" value="'.$row['description'].'">';
+      echo  '<input type="submit" name="submit" value="추가">';
+      echo '</form>';
+  	}
+
+    ?>
     </article>
   </body>
 </html>
