bbaca5b10d669d7ed4c216e81e44669e05ca7d5f
jisu An <ajs7270@naver.com>
2017-08-09 12:45:31 +0900

    fix process header url

--- process/reg_process.php
+++ process/reg_process.php
@@ -5,6 +5,6 @@ mysqli_select_db($conn,"mango");
 $sql = "INSERT INTO `mango`.`device` (`id`, `type`, `name`, `description`, `created`, `status`) VALUES (NULL, '-1','".$_POST['name']."','".$_POST['description']."',now(), '-1');";
 $result = mysqli_query($conn, $sql);
 
-header('Location: index.php?page=manage');
+header('Location: /index.php?page=manage');
 
 ?>
--- process/update_process.php
+++ process/update_process.php
@@ -6,5 +6,5 @@ mysqli_select_db($conn,"mango");
 $sql = "UPDATE `mango`.`device` SET `name` = '".$_POST['name']."', `description` = '".$_POST['description']."' WHERE `device`.`id` =".$_POST['id'].";";
 	
 $result = mysqli_query($conn, $sql);
-header('Location: index.php?page=manage');
+header('Location: /index.php?page=manage');
 ?>
