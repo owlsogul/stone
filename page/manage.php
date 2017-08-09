b341289ffae0baf956c4efea28837e90ab45f616
jisu An <ajs7270@naver.com>
2017-08-09 12:40:59 +0900

    refine process

--- page/manage.php
+++ page/manage.php
@@ -15,7 +15,7 @@ echo '로딩중...';
 // 이미 저장 되어 있는 기기일 경우
 echo '<h4>저장된 기기 목록</h4>';
 while($row=mysqli_fetch_assoc($result)){
-  echo '<form id="device_manage_info" class="" action="update_process.php" method="post">';
+  echo '<form id="device_manage_info" class="" action="process/update_process.php" method="post">';
   echo  '기기id   : '.$row['id'].'<br>';
   echo  '<input type=hidden name="id" value="'.$row['id'].'" >';
   echo	'기기타입 : '.$row['type'].'<br>';
@@ -32,7 +32,7 @@ while($row=mysqli_fetch_assoc($result)){
 
 //기기 추가(임의로 넣어줌)
 //추후에 주변에 있는 기기를 탐색해서 추가할 수 있는 방향으로
-echo '<form class="" action="reg_process.php" method="post">';
+echo '<form class="" action="process/reg_process.php" method="post">';
 echo  '<h2>기기추가</h2>';
 echo  '기기이름 : <input type="text" name="name" value="">';
 echo  '정보 : <input type="text" name="description" value="">';
