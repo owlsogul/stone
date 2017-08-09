4a45cf385964b6efc28d710e976c4552676fdedd
jisu An <ajs7270@naver.com>
2017-08-09 12:36:16 +0900

    edit header url on process

--- reg_process.php
+++ reg_process.php
@@ -2,11 +2,9 @@
 
 $conn=mysqli_connect("localhost","root","mango");
 mysqli_select_db($conn,"mango");
-
-$sql = "INSERT INTO `mango`.`device` (`id`, `name`, `description`, `created`, `status`) VALUES (NULL,'".$_POST['name']."','".$_POST['description']."',now(), NULL);";
-
+$sql = "INSERT INTO `mango`.`device` (`id`, `type`, `name`, `description`, `created`, `status`) VALUES (NULL, '-1','".$_POST['name']."','".$_POST['description']."',now(), '-1');";
 $result = mysqli_query($conn, $sql);
 
-header('Location: index.php');
+header('Location: index.php?page=manage');
 
 ?>
